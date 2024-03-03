b522a3526ea750d4094f72d7b489ce2198ac9929
Dieter Bergmann <dieterbergmann@arcor.de>
2024-03-03 07:35:20 +0100

    Initial Commit

---
+++ Project_7/Project_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_7App.swift
+//  Project_7
+//
+//  Created by Dieter Bergmann on 03.03.24.
+//
+
+import SwiftUI
+
+@main
+struct Project_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
